32817d210a59caf0f016ba61cafc52a983424a05
jovanaMilicevic <jovanamilicevic00@gmail.com>
2022-05-10 23:39:33 +0200

    verzija 3

--- Servis/servis.js
+++ Servis/servis.js
@@ -13,4 +13,45 @@ function readFiles()
     }).toString())
 }
 
-console.log(oglasi[0].Kategorija);
+exports.returnId = (id) =>
+{
+    return this.lista.find(a => a.Id == id);
+}
+
+exports.addOglas = (nov)=>{
+    let id=0;
+    if(this.lista.length>0)
+    {
+        id = this.lista[this.lista.length-1].Id+1;
+    }
+    nov.Id = id;
+    this.lista.push(nov);
+}
+
+exports.deleteOglas = (id) => {
+    this.lista = this.lista.filter(a => a.Id != id);
+}
+
+let nov = {
+    "Kategorija": "stanovi",
+    "DatumIsteka": "12.9.2022.",
+    "Cena": {
+        "valuta": "RSD",
+        "vrednost": 180.125
+    },
+    "Tekst": "Prodajem stan brt nzm sta ti nije jasno" ,
+    "Oznaka": [
+        "boze",
+        "pomozi"
+    ],
+    "Email": [
+        {
+            "email":"privatni",
+            "vrednost":"primer@gmail.com"
+        }
+    ]    
+}
+
+
+this.addOglas(nov);
+console.log(this.lista);
