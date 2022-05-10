99c8a89ba8c1aef4ec47313386a8eaf8e98ef71e
jovanaMilicevic <jovanamilicevic00@gmail.com>
2022-05-10 22:42:51 +0200

    verzija 2

--- Servis/servis.js
+++ Servis/servis.js
@@ -1,68 +1,16 @@
 const fs = require('fs');
-const lib = require('libxmljs2');
-const xml2js = require('xml2js');
+const path = "../oglasi.json";
 
-const pathXML = '../oglasi.xml';
-const pathDTD = '../oglasi.dtd';
-const pathXSD = '../oglasi.xsd';
+let oglasi = readFiles();
 
-let oglasi = [];
+exports.lista = oglasi;
 
-class Oglas 
+function readFiles()
 {
-    constructor(id, kategorija, datumIsteka, cene, tekst, oznake, emails)
-    {
-        this.id = id;
-        this.kategorija = kategorija;
-        this.datumIsteka = datumIsteka;
-        this.cene = cene;
-        this.tekst = tekst;
-        this.oznake = oznake;
-        this.emails = emails;
-    }
-}
-class Cena
-{
-    constructor(vrednost, valuta)
-    {
-        this.vrednost = vrednost;
-        this.valuta = valuta;
-    }
-}
-class Oznaka
-{
-    constructor(vrednost, oznaka)
-    {
-        this.vrednost = vrednost;
-        this.oznaka = oznaka;
-    }
-}
-class Email
-{
-    constructor(vrednost, email)
-    {
-        this.vrednost = vrednost;
-        this.email = email;
-    }
-}
-
-function readXML()
-{
-    let oglasXML;
-    let xml = fs.readFileSync(pathXML, (err, data)=>{
+    return JSON.parse(fs.readFileSync(path, (err, data)=>{
         if(err) throw err;
         return data;
-    }).toString();
-
-    xml2js.parseString(xml, (err, result) =>
-    {
-        if(err) throw err;
-        oglasXML = result['Oglasi']['Oglas'];
-    });
-
-    for(let oglas of oglasXML)
-    {
-        console.log(oglas.DatumIsteka[0]._);
-    }
+    }).toString())
 }
-readXML();
+
+console.log(oglasi[0].Kategorija);
