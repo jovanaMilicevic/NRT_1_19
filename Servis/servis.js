d18ab4ce587a3f97c2adb5a69071598856cec067
jovanaMilicevic <jovanamilicevic00@gmail.com>
2022-05-12 23:36:55 +0200

    verzija 4

--- Servis/servis.js
+++ Servis/servis.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = "../oglasi.json";
 
-let oglasi = readFiles();
+//let oglasi = readFiles();
 
-exports.lista = oglasi;
+exports.lista = readFiles();
 
 function readFiles()
 {
@@ -13,12 +13,17 @@ function readFiles()
     }).toString())
 }
 
+function saveFiles(data)
+{
+    fs.writeFileSync(path, JSON.stringify(data, null, 4));
+}
+
 exports.returnId = (id) =>
 {
     return this.lista.find(a => a.Id == id);
 }
 
-exports.addOglas = (nov)=>{
+exports.addOglas = (nov)=> {
     let id=0;
     if(this.lista.length>0)
     {
@@ -26,12 +31,28 @@ exports.addOglas = (nov)=>{
     }
     nov.Id = id;
     this.lista.push(nov);
+    saveFiles(this.lista);
 }
+
 
 exports.deleteOglas = (id) => {
     this.lista = this.lista.filter(a => a.Id != id);
+    saveFiles(this.lista);
 }
 
+exports.changeOglas = (nov) => {
+    this.lista[this.lista.findIndex(a => a.Id == this.lista.Id)] = nov;
+    saveFiles(this.lista);
+}
+exports.filtrirajPoKategoriji = (kategorija) => {
+    return this.lista.filter(a=>a.Kategorija.toLowerCase().includes(kategorija.toLowerCase()));
+}
+/*
+exports.filterOglas = (kat, val, cenOd, cenDo) => { 
+    return this.lista.filter(a => a.Kategorija.toLowerCase().includes(kat.toLowerCase()) && 
+        a.Cena.valuta.toLowerCase().includes(val.toLowerCase()) && a.Cena.cena >= cenOd && a.Cena.cena >= cenDo);
+}*/
+/*
 let nov = {
     "Kategorija": "stanovi",
     "DatumIsteka": "12.9.2022.",
@@ -54,4 +75,4 @@ let nov = {
 
 
 this.addOglas(nov);
-console.log(this.lista);
+console.log(this.lista);*/
