de8a677e4332d6320320e8c0c1514f9c8bc89c47
jovanaMilicevic <jovanamilicevic00@gmail.com>
2022-05-13 03:41:28 +0200

    verzija 2

--- Klijent/klijent.js
+++ Klijent/klijent.js
@@ -20,8 +20,13 @@ app.get("/", (req, res) =>{
 app.get("/sviOglasi", (req,res) => {
     axios.get('http://localhost:3000/Oglasi')
     .then(response => {
+        let oznakeHTML="";
+        console.log(response.data);
         let prikaz = "";
         response.data.forEach(element => {
+        oznakeHTML="";
+        element.Oznaka.forEach(o=>oznakeHTML+=`<label type="text" name="oznaka" value="${o}"></label>`)
+            
             prikaz += 
             `
                 <tr>
@@ -31,14 +36,17 @@ app.get("/sviOglasi", (req,res) => {
                     <td>${element.Cena.valuta}</td>
                     <td>${element.Cena.vrednost}</td>
                     <td>${element.Tekst}</td>
-                    <td>${element.Oznaka}</td> 
+                    <td>
+                        <div id="oznake">${oznakeHTML}</div>
+                    </td>
                     <td>${element.Email}</td>
-                    <td><a href="/detaljnije/${element.Id}">Izmeni</a></td>
+                    <td><a href="/izmeni/${element.Id}">Izmeni</a></td>
                     <td><a href="/obrisi/${element.Id}">Obrisi</a></td>
                 </tr>
+
             `
         }); //fali oznaka, fali email
-        res.send(procitajPogledZaNaziv("sviOglasi").replace("#{data}", prikaz));
+        res.send(procitajPogledZaNaziv("/sviOglasi").replace("#{data}", prikaz));
     })
     .catch(error =>{
         console.log(error);
@@ -56,9 +64,9 @@ app.get("/addOglas", (req,res) =>{
     res.send(procitajPogledZaNaziv("formaZaDodavanje"));
 });
 
-app.get("/snimiOglas", (req,res) => {
-    axios.post("http://localhost:3000/addOglas", {
-        Id:0,
+app.post("/snimiOglas", (req,res) => {
+    axios.post("http://localhost:3000/changeOglas", {
+        Id:1,
         Kategorija:req.body.kategorija,
         DatumIsteka:req.body.datumIsteka,
         Cena:
@@ -67,11 +75,54 @@ app.get("/snimiOglas", (req,res) => {
             vrednost:req.body.cena
         },
         Tekst:req.body.tekst,
-        Oznaka:req.body.oznaka,
-        Email:req.body.email
-    });//oglasi i email nije kako treba............ne radi nista kako treba
+        Oznaka:[req.body.oznaka],
+        Email:[{
+            email:req.body.email,
+            vrednost: req.body.vrednost
+        }]
+        
+    });//oglasi i email nije kako treba
     res.redirect("/sviOglasi");
 });
+
+app.get("/izmeni/:Id", (req, res) =>{
+    axios.get(`http://localhost:3000/returnId/${req.params["Id"]}`).then(response=>{
+        let prikaz =
+        `
+            <input type="number" name="Id" value=${response.data.Id} hidden>
+            <label>Kategorija:</label> <select name="kategorija">
+                                            <option value="${response.data.Kategorija}">${response.data.Kategorija}</option>
+                                            <option value="automobili">Automobili</option>
+                                            <option value="stanovi">Stanovi</option>
+                                            <option value="alati">Alati</option>
+                                            <option value="motori">Motori</option>
+                                            <option value="elektronika">Elektronika</option>
+                                        </select>
+            <br>
+            <label>Datum isteka oglasa:</label> <input type="date" name="expiringDate" value="${response.data.DatumIsteka}">
+            <label>Cena:</label> <input type="number" name="vrednost" value="${response.data.Cena.vrednost}"> <select name="valuta">
+                                                                                                                    <option value="RSD">RSD</option>
+                                                                                                                    <option value="EUR">EUR</option>
+                                                                                                                </select>
+            <label>Tekst:</label> 
+            <input type="text" name="tekst" value="${response.data.Tekst}">
+            <br>
+            <label>Oznake:</label>
+            <input type="text" name="oznaka" value="${response.data.Oznaka}">
+            <label>Email:</label><input type="text" name="email" value="${response.data.Email[0].vrednost}"><select name="tipMaila">
+                                                                                                    <option value="${response.data.Email[0].email}">${response.data.Email[0].email}</option>
+                                                                                                    <option value="privatni">Privatni</option>
+                                                                                                    <option value="sluzbeni">Sluzbeni</option>
+                                                                                                </select>
+            
+        `
+        res.send(procitajPogledZaNaziv('izmena').replace("#{data}",prikaz));
+    }).catch(error=>{ //fali za oznake i email, email nece da se prikaze vrednost tipa, sad sam i to sjebala
+        console.log(error);
+    });
+});
+
+
 
 app.post("/filtrirajPoKategoriji", (req,res) => {
     axios.get(`http://localhost:3000/filtrirajPoKategoriji?kategorija=${req.body.filter}`).then(response => {
@@ -88,7 +139,7 @@ app.post("/filtrirajPoKategoriji", (req,res) => {
                     <td>${element.Tekst}</td>
                     <td>${element.Oznaka}</td>
                     <td>${element.Email}</td>
-                    <td><a href="/detaljnije/${element.Id}">Izmeni</a></td>
+                    <td><a href="/izmeni/${element.Id}">Izmeni</a></td>
                     <td><a href="/obrisi/${element.Id}">Obrisi</a></td>
                 </tr>
             `
@@ -97,6 +148,24 @@ app.post("/filtrirajPoKategoriji", (req,res) => {
     }).catch(error => {
         console.log(error);
     });
+});
+
+app.post("/changeOglas", (req,res)=>{
+    axios.post("http://localhost:3000/changeOglas",{
+        Id:req.body.Id,
+        Kategorija:req.body.kategorija,
+        DatumIsteka:req.body.expiringDate,
+        Cena:{
+            valuta:req.body.valuta,
+            vrednost:req.body.vrednost
+        },
+        Tekst:req.body.tekst,
+        Oznaka:[req.body.oznaka],
+        Email:[{email:req.body.email,
+            vrednost: req.body.vrednost}]
+
+    })
+    res.redirect('/sviOglasi')
 });
 
 app.listen(port, ()=>{
